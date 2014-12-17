9d003dd4c46599706dd4d097ce25ecafcf56cc3d
ajwhi7 <adamw100@hotmail.com>
2014-12-17 12:17:09 +1100

    Finish adding admin to nodes

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,4 +1,6 @@
 class NodesController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update, :destroy]
+	before_action :admin_user,     only: :destroy
 	def show
 		@node = Node.find(params[:id])
 	end
@@ -19,10 +21,34 @@ class NodesController < ApplicationController
 	end
 
 	def edit
-    	@node = Node.find
+    	@node = Node.find(params[:id])
   	end
+
+  	def update
+    	@node = Node.find(params[:id])
+    	if @node.update_attributes(node_params)
+    	  flash[:success] = "Node refined"
+    	  redirect_to @node
+    	else
+   		   render 'edit'
+    	end
+  	end
+  
+  def destroy
+    Node.find(params[:id]).destroy
+    flash[:success] = "Node deleted."
+    redirect_to root_url
+  end
+
     def node_params
       params.require(:node).permit(:title, :what, :when)
     end
 
+# Before filters
+    def signed_in_user
+    	unless signed_in?
+        store_location
+      	redirect_to signin_url, notice: "Please sign in to contribute."
+    	end
+	end
 end
